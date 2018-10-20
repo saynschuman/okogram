9bbce6e08ce32a1c367ea20b7daa38b460ae75d9
saynschuman <saynschuman@gmail.com>
2018-10-20 12:00:22 +0300

    basic calendar works

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,57 +1,22 @@
-import React from "react";
 import BigCalendar from "react-big-calendar";
+import React from "react";
 import events from "./events";
-import ExampleControlSlot from "./ExampleControlSlot";
+import moment from "moment";
+import "./Calendar.css";
 
-const propTypes = {};
+const localizer = BigCalendar.momentLocalizer(moment);
 
-class Selectable extends React.Component {
-  constructor(...args) {
-    super(...args);
-
-    this.state = { events };
-  }
-
-  handleSelect = ({ start, end }) => {
-    const title = window.prompt("New Event name");
-    if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title
-          }
-        ]
-      });
-  };
-
+class Calendar extends React.Component {
   render() {
-    const { localizer } = this.props;
     return (
-      <>
-        <ExampleControlSlot.Entry waitForOutlet>
-          <strong>
-            Click an event to see more info, or drag the mouse over the calendar
-            to select a date/time range.
-          </strong>
-        </ExampleControlSlot.Entry>
-        <BigCalendar
-          selectable
-          localizer={localizer}
-          events={this.state.events}
-          defaultView={BigCalendar.Views.MONTH}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date(2015, 3, 12)}
-          onSelectEvent={event => alert(event.title)}
-          onSelectSlot={this.handleSelect}
-        />
-      </>
+      <BigCalendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     );
   }
 }
 
-Selectable.propTypes = propTypes;
-
-export default Selectable;
+export default Calendar;
