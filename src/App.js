2a307ceb29fc75aa8587c9b29652a533095b1ea8
saynschuman <saynschuman@gmail.com>
2018-10-20 11:39:55 +0300

    formats

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Counter from "./components/counter";
+import Calendar from "./components/Calendar/Calendar";
 
 class App extends React.Component {
   render() {
-    return <Counter />;
+    return (
+      <div>
+        <Counter />
+        <Calendar />
+      </div>
+    );
   }
 }
 
